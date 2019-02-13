b34e53ccca06dfae58d038d3862519b4133bcd85
Teamsushie <teamsushie@gmail.com>
2019-02-13 16:53:59 -0700

    big update for the website. 1.1

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -1,4 +1,12 @@
-console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55');
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 /* ---------------------------------------------- /*
  * Preloader
